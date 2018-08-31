7db1323f60ff30ed5c5b4bd5cd4cf26bc138c265
Andy Iwamoto <aiwamoto789@gmail.com>
2018-08-31 17:24:55 -0600

    Updated comment line main method

--- src/com/skilldistillery/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/makechange/MakeChangeApp.java
@@ -3,7 +3,13 @@ package com.skilldistillery.makechange;
 public class MakeChangeApp {
 
 	public static void main(String[] args) {
-		
+		//Make a scanner
+		//Prompt for price
+		//get price from user
+		//accept user submitted cash amount
+		//if not enough cash, ask for appropriate additional amount
+		//make change from price and submitted cash amount
+		//thank user for patronage
 
 	}
 
